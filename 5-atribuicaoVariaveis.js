680c8fb62a574c69218b4b4ec5b6e56c6747c5f2
auanymen <127303875+auanymen@users.noreply.github.com>
2023-08-04 09:10:15 -0300

    Add files via upload

---
+++ 5-atribuicaoVariaveis.js
@@ -0,0 +1,10 @@
+console.log("Trabalhando com atribuição de variáveis");
+const idade = 29;
+const nome = "Ricardo";
+const sobrenome = "Bugan"; 
+
+//console.log(nome + " " + sobrenome);
+console.log(nome + sobrenome);
+console.log(`Meu nome é ${nome} ${sobrenome}`);
+
+nome = nome + sobrenome;
